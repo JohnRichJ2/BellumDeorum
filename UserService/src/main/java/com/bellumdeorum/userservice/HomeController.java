14520a058bcbc08a211c493fa1d41a82a17279aa
John <johnrichj2@gmail.com>
2013-02-28 23:14:49 -0800

    asd

--- UserService/src/main/java/com/bellumdeorum/userservice/HomeController.java
+++ UserService/src/main/java/com/bellumdeorum/userservice/HomeController.java
@@ -4,13 +4,18 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Handles requests for the application home page.
@@ -40,9 +45,9 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	@ResponseBody
-	public String hello(Locale locale, Model model) {
+	public String hello(HttpServletRequest request, HttpServletResponse response, Locale locale, ModelMap model) {
 		return "{\"name\":\"hello\"}";
 	}
 	
