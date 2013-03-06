9c2e0554b47e6e35d09b3ee6eb2c1b28805d3db8
John <johnrichj2@gmail.com>
2013-03-05 23:30:50 -0800

    json Parameter, yay!

--- BellumDeorumWebsite/src/main/java/com/bellumdeorum/website/controllers/HomeController.java
+++ BellumDeorumWebsite/src/main/java/com/bellumdeorum/website/controllers/HomeController.java
@@ -1,15 +1,21 @@
 package com.bellumdeorum.website.controllers;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.bellumdeorum.userservice.User;
@@ -18,6 +24,17 @@ import com.bellumdeorum.userservice.User;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	private final ObjectMapper mapper;
+	
+	protected HomeController() {
+		this.mapper = null;
+	}
+	
+	@Autowired
+	public HomeController(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
@@ -31,7 +48,26 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		RestTemplate restTemplate = new RestTemplate();
-		User response = (User)restTemplate.getForObject("http://localhost:8080/userservice/user", User.class);
+		User user = new User();
+		user.setName("John");
+		
+		User response = new User();
+		try {
+			System.out.println(String.format("http://localhost:8080/userservice/user/%s", mapper.writeValueAsString(user)));
+			response = (User)restTemplate.getForObject("http://localhost:8080/userservice/user/{user}", User.class, mapper.writeValueAsString(user));
+		} catch (RestClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		System.out.println(response.getName().toString());
 		
--- UserService/src/main/java/com/bellumdeorum/userservice/HomeController.java
+++ UserService/src/main/java/com/bellumdeorum/userservice/HomeController.java
@@ -1,5 +1,6 @@
 package com.bellumdeorum.userservice;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -7,11 +8,16 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,6 +30,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private final ObjectMapper mapper;
+	
+	protected HomeController() {
+		this.mapper = null;
+	}
+	
+	@Autowired
+	public HomeController(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -45,10 +61,33 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	@RequestMapping(value = "/user/{json}", method = RequestMethod.GET)
 	@ResponseBody
-	public String hello(HttpServletRequest request, HttpServletResponse response, Locale locale, ModelMap model) {
-		return "{\"name\":\"hello\"}";
+	public User hello(HttpServletRequest request, HttpServletResponse response, Locale locale, ModelMap model,
+			@PathVariable("json") String json) {
+		
+		System.out.println(json);
+		System.out.println("made it here!!!");
+		
+		try {
+			User user = mapper.readValue(json, User.class);
+			System.out.println(user.getName());
+			return user;
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		User user = new User();
+		user.setName("JohnYEah");
+		
+		return user;
 	}
 	
 }
